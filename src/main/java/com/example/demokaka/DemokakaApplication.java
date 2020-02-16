6725b64b7926a792d6958dd17a8346c444567ada
👾 叉烧包 <shujie@deMacBook-Pro.local>
2020-02-16 11:36:09 +0800

    Initial commit

---
+++ src/main/java/com/example/demokaka/DemokakaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demokaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemokakaApplication.class, args);
+    }
+
+}
